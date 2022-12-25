787416dddcb99a96a8265580798777fab45202d2
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-12-25 22:47:25 +0000

    understanding object and enums in typescript

--- dist/objects&Enums.js
+++ dist/objects&Enums.js
@@ -1,2 +1,32 @@
 "use strict";
+var _a;
 //Object and Enums in TypeScript
+var player;
+player = {
+    name: 'messi',
+    age: 36,
+    married: 1,
+    clubs: ['barca', 'psg'],
+    country: 'argentina'
+};
+player.age = player.age + 5; /* to change any optional property value */
+player.clubs = (_a = player.clubs) === null || _a === void 0 ? void 0 : _a.map(function (club) { return club.toUpperCase(); }); /* to change any optional array property value */
+/* ***************************************** */
+var friends;
+(function (friends) {
+    friends[friends["ami"] = 0] = "ami";
+    friends[friends["tumi"] = 1] = "tumi";
+    friends[friends["he"] = 2] = "he";
+    friends[friends["she"] = 3] = "she";
+})(friends || (friends = {}));
+(function (friends) {
+    friends[friends["Ami"] = 10] = "Ami";
+    friends[friends["Tumi"] = 7] = "Tumi";
+    friends[friends["He"] = 2] = "He";
+    friends[friends["She"] = 9] = "She";
+})(friends || (friends = {}));
+var secrect;
+(function (secrect) {
+    secrect["API_Key"] = "api key";
+    secrect["Stripe_Key"] = "st0458";
+})(secrect || (secrect = {}));
--- src/objects&Enums.ts
+++ src/objects&Enums.ts
@@ -1,1 +1,39 @@
 //Object and Enums in TypeScript
+let player: {
+    name: string;
+    age?: number; /* to make any property optional */
+    married: boolean | number;
+    clubs?: string[]; /* to make any property optional */
+    country: string;
+}
+
+player = {
+    name: 'messi',
+    age: 36,
+    married: 1,
+    clubs: ['barca', 'psg'],
+    country: 'argentina'
+}
+
+player.age = player.age! + 5; /* to change any optional property value */
+player.clubs = player.clubs?.map(club => club.toUpperCase()); /* to change any optional array property value */
+
+/* ***************************************** */
+
+enum friends { /* enum is a kind of object */
+    ami,
+    tumi,
+    he,
+    she
+}
+enum friends { /* a value could be assigned */
+    Ami = 10,
+    Tumi = 7,
+    He = 2,
+    She = 9
+}
+
+enum secrect { /* value can not be changed */
+    API_Key = 'api key',
+    Stripe_Key = 'st0458'
+}
